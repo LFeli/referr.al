f11b877d64b2e93f591aca20ce50abc3b1bd3070
Lucas Araújo <lucasfelipemk@gmail.com>
2025-02-20 21:50:20 -0300

    :wrench: chore: configured fastify cors + integration of fastify with zod for validation and serializer

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,15 @@
 import fastify from "fastify";
+import { fastifyCors } from "@fastify/cors"
+import { validatorCompiler, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+  origin: "http://localhost:3000",
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running! 🚀...')
