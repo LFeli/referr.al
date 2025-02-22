7a9d9042a2473cf3936970cb3db1400f9430d913
Lucas Araújo <lucasfelipemk@gmail.com>
2025-02-22 16:00:59 -0300

    :lipstick: apply eslint-simple-import-sort on server.ts file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,13 @@
-import fastify from 'fastify'
 import { fastifyCors } from '@fastify/cors'
-import {
-  validatorCompiler,
-  serializerCompiler,
-  ZodTypeProvider,
-  jsonSchemaTransform,
-} from 'fastify-type-provider-zod'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
