725c8519de9b2e4af407bbdeaf34c2017669073f
Lucas Araújo <lucasfelipemk@gmail.com>
2025-02-22 15:57:05 -0300

    :lipstick: run eslint in server.ts file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,13 @@
-import fastify from "fastify";
-import { fastifyCors } from "@fastify/cors"
-import { validatorCompiler, serializerCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import fastify from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform,
+} from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -8,9 +15,24 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors, {
-  origin: "http://localhost:3000",
+  origin: 'http://localhost:3000',
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'reffer.al',
+      version: '0.0.1',
+    },
+  },
+
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
 })
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('Server running! 🚀...')
+  console.log('Server running... 🚀')
 })
