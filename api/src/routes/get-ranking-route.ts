1c88ad610ee8cc6dd62ff95943a7e07cb2f3caf7
Lucas Araújo <lucasfelipemk@gmail.com>
2025-02-23 13:49:37 -0300

    :sparkles: feat: implemented and created route for a function get-ranking

---
+++ api/src/routes/get-ranking-route.ts
@@ -0,0 +1,37 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { getRanking } from '../drizzle/functions/get-ranking'
+import { getSubscriberInviteCount } from '../drizzle/functions/get-subscriber-invite-count'
+
+export const getRankingRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/ranking',
+    {
+      schema: {
+        tags: ['referral'],
+        summary: 'Get ranking',
+        params: z.object({
+          subscriberID: z.string(),
+        }),
+        response: {
+          200: z.object({
+            ranking: z.array(
+              z.object({
+                id: z.string(),
+                name: z.string(),
+                score: z.number(),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { rankingWithScore } = await getRanking()
+
+      return reply.status(200).send({
+        ranking: rankingWithScore,
+      })
+    }
+  )
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -11,6 +11,7 @@ import {
 
 import { env } from './env'
 import { accessInviteLinkRoute } from './routes/access-invite-link-route'
+import { getRankingRoute } from './routes/get-ranking-route'
 import { getSubscriberInviteClicksRoute } from './routes/get-subscriber-invite-clicks-route'
 import { getSubscriberInviteCountRoute } from './routes/get-subscriber-invites-count-route'
 import { getSubscriberRankingPositionRoute } from './routes/get-subscriber-ranking-position-route'
@@ -45,6 +46,7 @@ app.register(accessInviteLinkRoute)
 app.register(getSubscriberInviteClicksRoute)
 app.register(getSubscriberInviteCountRoute)
 app.register(getSubscriberRankingPositionRoute)
+app.register(getRankingRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server running... 🚀')
