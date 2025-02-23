e998629946b5177b709b248f8df49c5b6fa99021
Lucas Araújo <lucasfelipemk@gmail.com>
2025-02-23 12:20:42 -0300

    :sparkles: feat: created access-invite-link route in API

---
+++ api/src/routes/access-invite-link-route.ts
@@ -0,0 +1,34 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { accessInviteLink } from '../drizzle/functions/access-invite-link'
+import { env } from '../env'
+
+export const accessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/invites/:subscriberID',
+    {
+      schema: {
+        tags: ['referral'],
+        summary: 'Access invite link and redirect user',
+        params: z.object({
+          subscriberID: z.string(),
+        }),
+        response: {
+          201: z.object({
+            subscriberID: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { subscriberID } = request.params
+
+      await accessInviteLink({ subscriberID })
+
+      const redirectUrl = new URL(env.WEB_URL)
+      redirectUrl.searchParams.set('referrer', subscriberID)
+
+      return reply.redirect(redirectUrl.toString(), 302)
+    }
+  )
+}
