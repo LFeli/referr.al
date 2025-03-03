29a018e21bb3dd380fe0a2ab48ea86e47afe5294
Lucas Araújo <lucasfelipemk@gmail.com>
2025-03-03 19:48:17 -0300

    :sparkles: feat: integrated ranking components usign API data

--- web/src/app/invite/[subscriberID]/components/ranking.tsx
+++ web/src/app/invite/[subscriberID]/components/ranking.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 
+import { getRanking } from '@/http/api'
 import cooper from '../../../../assets/medal-cooper.svg'
 import gold from '../../../../assets/medal-gold.svg'
 import silver from '../../../../assets/medal-silver.svg'
 
-export function Ranking() {
+export async function Ranking() {
+  const { ranking } = await getRanking()
+
   return (
     <section className="w-full max-w-[440px] space-y-5">
       <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
@@ -12,53 +15,49 @@ export function Ranking() {
       </h2>
 
       <div className="space-y-4">
-        <article className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">1º</span> | Diego Fernandes
-          </span>
+        {ranking.map((user, index) => {
+          const rankingPosition = index + 1
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+          return (
+            <article
+              key={user.id}
+              className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3"
+            >
+              <span className="text-sm text-gray-300 leading-none">
+                <span className="font-semibold">{rankingPosition}º</span> |{' '}
+                {user.name}
+              </span>
 
-          <Image
-            src={gold}
-            alt="illustration of a gold medal"
-            className="absolute top-0 right-8"
-          />
-        </article>
+              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+                {user.score}
+              </span>
 
-        <article className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">2º</span> | Diego Fernandes
-          </span>
+              {rankingPosition === 1 && (
+                <Image
+                  src={gold}
+                  alt="illustration of a gold medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
+              {rankingPosition === 2 && (
+                <Image
+                  src={silver}
+                  alt="illustration of a silver medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
 
-          <Image
-            src={silver}
-            alt="illustration of a silver medal"
-            className="absolute top-0 right-8"
-          />
-        </article>
-
-        <article className="relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justify-center gap-3">
-          <span className="text-sm text-gray-300 leading-none">
-            <span className="font-semibold">3º</span> | Diego Fernandes
-          </span>
-
-          <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-            1030
-          </span>
-
-          <Image
-            src={cooper}
-            alt="illustration of a cooper medal"
-            className="absolute top-0 right-8"
-          />
-        </article>
+              {rankingPosition === 3 && (
+                <Image
+                  src={cooper}
+                  alt="illustration of a cooper medal"
+                  className="absolute top-0 right-8"
+                />
+              )}
+            </article>
+          )
+        })}
       </div>
     </section>
   )
